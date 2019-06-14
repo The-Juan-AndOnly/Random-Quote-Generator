fa5c7c16abb07cd1fd2935057a6d02b58123796a
Juan Lopez <juanl0215@gmail.com>
2019-06-14 16:32:10 -0400

    Update functionality of printQuote

--- js/script.js
+++ js/script.js
@@ -61,69 +61,58 @@ const quotes = [
 
 // RandomQuote function that returns a random quote
 const getRandomQuote = () => {
-  // randomNumber variable assigned to a random number whcih will be between 0 and quotes.length
   let randomNumber = Math.floor(Math.random() * quotes.length);
-  // return the value of the quotes araay at the randomNumber position
   return quotes[randomNumber];
 };
 
-// Change background color of the body element
+// Function that will change the body and loadQuote button background color
 const randomColor = () => {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  document.querySelector('body').style.backgroundColor = `rgb(${r},${g},${b})`;
-};
 
-// getQuotes object that will determine if our interval is running or not
-let getQuotes = {
-  isRunning: false,
-  interval: ''
+  document.querySelector('body').style.backgroundColor = `rgb(${r},${g},${b})`;
+  document.querySelector('#loadQuote').style.backgroundColor = `rgb(${r /
+    2},${g / 2},${b / 2})`;
 };
 
 /*  printQuote function that calls the randomQuote function
     assigns an empty string to quoteString and cumulatively assigns the quote properties also checks to make sure that a year and citation was provided
 */
 const printQuote = () => {
-  // if our interval is already running it will clear the Interval, change the button text back to default and reset isRunning back to false
-  if (getQuotes.isRunning) {
-    clearInterval(getQuotes.interval);
-    document.querySelector('#loadQuote').textContent = 'Show another quote';
-    getQuotes.isRunning = false;
-  } else {
-    // set our isRunning to true to prevent user from clicking multiple times
-    getQuotes.isRunning = true;
-    // setInterval for quotes to update every 3 seconds
-    getQuotes.interval = setInterval(() => {
-      //Get Quote Object and assign it to quote variable
-      const quote = getRandomQuote();
-      // empty quote string assigned to the variable
-      let quoteString = '';
-      quoteString += `<p class="quote">${quote.quote}</p>`;
-      quoteString += `<p class="source">${quote.source}`;
-      // Conditional to check if a citation was provided if so will assign it to the quoteString
-      if (quote.citation !== '') {
-        quoteString += `<span class="citation">${quote.citation}</span>`;
-      }
-      // Conditional to check if a year was provided if so will assign it to the quoteString
-      if (quote.year !== '' && quote.year !== null) {
-        quoteString += `<span class="year">${quote.year}</span>`;
-      }
-      // Condition to check if the "tag" is inspirational which will display along with the quote
-      if (quote.tag === 'inspirational') {
-        quoteString += ` <sup class="tag">${quote.tag}</sup>`;
-      }
-      quoteString += `</p>`;
-      document.querySelector('#quote-box').innerHTML = quoteString;
-      // Change button text
-      document.querySelector('#loadQuote').textContent = 'Click to stop';
-      randomColor();
-    }, 3000);
-    document.querySelector('#loadQuote').textContent = 'Retrieving quotes...';
+  //Get Quote Object and assign it to quote variable
+  const quote = getRandomQuote();
+  // empty quote string assigned to the variable
+  let quoteString = '';
+  // Concatenate the quote to quoteString
+  quoteString += `<p class="quote">${quote.quote}</p>`;
+  quoteString += `<p class="source">${quote.source}`;
+
+  // Conditional to check if a citation was provided if so will assign it to the quoteString
+  if (quote.citation) {
+    quoteString += `<span class="citation">${quote.citation}</span>`;
   }
+
+  // Conditional to check if a year was provided if so will assign it to the quoteString
+  if (quote.year !== '' && quote.year !== null) {
+    quoteString += `<span class="year">${quote.year}</span>`;
+  }
+
+  // Condition to check if the "tag" is inspirational which will display along with the quote
+  if (quote.tag === 'inspirational') {
+    quoteString += ` <sup class="tag">${quote.tag}</sup>`;
+  }
+
+  quoteString += `</p>`;
+  document.querySelector('#quote-box').innerHTML = quoteString;
+
+  randomColor();
 };
 
-// EventListener for when the "Show another quote" button will fire off the printQuote function
+// Set an interval for a new quote to load every 10 seconds
+setInterval(printQuote, 10000);
+
+// EventListener to fire off the printQuote function
 document
   .getElementById('loadQuote')
   .addEventListener('click', printQuote, false);
